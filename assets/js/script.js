7328a3532def9fbb748b4301cdf2063497d8aa3c
Derrick Lafforthun <derricklaff@gmail.com>
2022-09-27 19:43:21 -0700

    code functions but getting error on line 45

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
+/* CallBack function */
 function render() {
     localStorage.removeItem('cities');
     /* When the page refresh, it's clear the local storage up. */
 
     /* Given City by the user */
-    var cityPicked = "";
+    let cityPicked = "";
     /* storage */
-    var citySearched = JSON.parse(localStorage.getItem("cities")) || [];
+    let citySearched = JSON.parse(localStorage.getItem("cities")) || [];
     /* actual date */
     const date = moment().format("MMMM Do YYYY");
 
@@ -19,33 +20,34 @@ function render() {
             }).then(function (data) {
                 console.log(data);
                 $("#fetch").empty();
-                var today = data;
-                var jumbo = document.getElementById("#fetch");
-                var infoJumbo = $("<div>");
-                var day = $("#fetch-elm").addClass("date-of");
-                var dayOf = $("<h4>").addClass("actual-date").text(date);
-                var expCity = $("<p>").addClass("exp-city").text(" Weather for: ").append(cityPicked);
-                var degree = $("<p>").addClass("lead").text(Math.round(today.main.temp) + "˚F");
-                var humid = $("<p>").addClass("humidity").text("Humidity: " + today.main.humidity + "%");
-                var wind = $("<p>").addClass("wind").text("Wind Speed: " + Math.round(today.wind.speed) + " mph");
-                var icon = $("<img>").addClass("rounded mx-auto d-block");
+                let today = data;
+                let jumbo = document.getElementById("#fetch");
+                let infoJumbo = $("<div>");
+                let day = $("#fetch-elm").addClass("date-of");
+                let dayOf = $("<h4>").addClass("actual-date").text(date);
+                let expCity = $("<p>").addClass("exp-city").text(" Weather for: ").append(cityPicked);
+                let degree = $("<p>").addClass("lead").text(Math.round(today.main.temp) + "˚F");
+                let humid = $("<p>").addClass("humidity").text("Humidity: " + today.main.humidity + "%");
+                let wind = $("<p>").addClass("wind").text("Wind Speed: " + Math.round(today.wind.speed) + " mph");
+                let icon = $("<img>").addClass("rounded mx-auto d-block");
                 icon.attr("src", "http://openweathermap.org/img/wn/" + today.weather[0].icon + "@2x.png");
-                var iconMain = $("<p>").text(today.weather[0].main).addClass("iconexp");
+                let iconMain = $("<p>").text(today.weather[0].main).addClass("iconexp");
                 console.log(today.weather[0].icon);
                 $("#fetch").append(infoJumbo.append(day, dayOf, expCity, degree, humid, wind, icon, iconMain));
                 $("#one-day").append(jumbo);
 
+
                 /* Uv Index Function */
-                var latitude = today.coord.lat
-                var longtitude = today.coord.lon;
+                let latitude = today.coord.lat
+                let longtitude = today.coord.lon;
                 function uvIndex() {
-                    const uvUrl = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longtitude + "&appid=929ca2e9670ab5d91ac901590336e78c";
+                    const uvUrl = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longtitude + "&appid=929ca2e9670ab5d91ac901590336e78cs";
                     fetch(uvUrl)
                         .then(function (datas) {
                             return datas.json();
                         }).then(function (uvInfo) {
                             console.log(uvInfo.value);
-                            var uvIn = $("<p>");
+                            let uvIn = $("<p>");
                             uvIn.text("UV index: " + uvInfo.value).addClass("uvdesign");
                             if (uvInfo.value <= 3) {
                                 uvIn.addClass("great").text("UVindex: " + uvInfo.value + " / Favorable");
@@ -62,55 +64,79 @@ function render() {
                 uvIndex();
             })
     }
-}
 
-function fiveDaysApi() {
-    const urlRequest = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityPicked + "&units=imperial&appid=45b6598a4a1bd706ba39bf0f2ac2fcf4";
+    /* Five days Forecast Function */
+    function fiveDaysApi() {
+        const urlRequest = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityPicked + "&units=imperial&appid=45b6598a4a1bd706ba39bf0f2ac2fcf4";
 
-    fetch(urlRequest)
-        .then(function (data) {
-            return data.json();
-        }).then(function (data) {
-            $("#fetch-five").empty();
-            console.log(data);
-            for (var i = 0; i < 40; i += 8) {
-                var days = data.list[i];
-                /* console.log(data.list[0].dt_txt); */
-                var cards = document.getElementById("#fetch-five")
-                var cardInit = $("<div>").addClass("col-sm-2 whole");
-                var cardDay = $("<h2>").text(days.dt_txt.slice(0, 10));
-                /* console.log(data.list[0].main.temp + "˚F"); */
-                var degrees = $("<p>").text(Math.round(days.main.temp) + "˚F");
-                /* console.log(data.list[0].main.humidity + " %"); */
-                var humidity = $("<p>").text("Humidity: " + days.main.humidity + "%");
-                /* console.log(data.list[0].wind.speed + " mph"); */
-                var wind = $("<p>").text("wind Speed: " + Math.round(days.wind.speed) + " mph");
-                var icon = $("<img>");
-                icon.attr("src", "http://openweathermap.org/img/wn/" + days.weather[0].icon + "@2x.png");
-                $("#fetch-five").append(cardInit.append(cardDay, degrees, icon, humidity, wind));
-                $("#five-days").append(cards);
-            }
-        })
-}
-/* City Searched history function that spawns the cities under the submit form */
-function searchedCities() {
-    $("#searched").empty();
-    for (var i = 0; i < citySearched.length; i++) {
-        var el = $("<p class='cities'>");
-        el.attr("data", citySearched[i]);
-        el.text(citySearched[i]);
-        $("#searched").append(el);
+        fetch(urlRequest)
+            .then(function (data) {
+                return data.json();
+            }).then(function (data) {
+                $("#fetch-five").empty();
+                console.log(data);
+                for (let i = 0; i < 40; i += 8) {
+                    let days = data.list[i];
+                    /* console.log(data.list[0].dt_txt); */
+                    let cards = document.getElementById("#fetch-five")
+                    let cardInit = $("<div>").addClass("col-sm-2 whole");
+                    let cardDay = $("<h2>").text(days.dt_txt.slice(0, 10));
+                    /* console.log(data.list[0].main.temp + "˚F"); */
+                    let degree = $("<p>").text(Math.round(days.main.temp) + "˚F");
+                    /* console.log(data.list[0].main.humidity + " %"); */
+                    let humid = $("<p>").text("Humidity: " + days.main.humidity + "%");
+                    /* console.log(data.list[0].wind.speed + " mph"); */
+                    let wind = $("<p>").text("wind Speed: " + Math.round(days.wind.speed) + " mph");
+                    let icon = $("<img>");
+                    icon.attr("src", "http://openweathermap.org/img/wn/" + days.weather[0].icon + "@2x.png");
+                    $("#fetch-five").append(cardInit.append(cardDay, degree, icon, humid, wind));
+                    $("#five-days").append(cards);
+                }
+            })
+    }
+
+    /* City Searched history function that spawns the cities under the submit form */
+    function searchedCities() {
+        $("#searched").empty();
+        for (let i = 0; i < citySearched.length; i++) {
+            let el = $("<p class='cities'>");
+            el.attr("data", citySearched[i]);
+            el.text(citySearched[i]);
+            $("#searched").append(el);
+
+        }
 
     }
 
+    /* Click function, it saves searched cities and runs the forecast app.. */
+    $("#submitBtn").on("click", function (event) {
+        event.preventDefault();
+        cityPicked = $("#given-input").val().trim();
+
+        if (!citySearched.includes(cityPicked)) {
+            (citySearched).push(cityPicked);
+        }
+        if (citySearched.length > 5) {
+            citySearched.shift();
+        }
+
+        searchedCities();
+        getApi();
+        fiveDaysApi();
+        localStorage.setItem("cities", JSON.stringify(citySearched));
+        $("#given-input").val("");
+        $("#issues").css('display', 'block');
+    });
+
+    searchedCities();
+
+    /* Click function, it gets the cities saved under the submit form and runs the forecast app again */
+    $(document).on("click", ".cities", function () {
+        cityPicked = $(this).text();
+        $(cityPicked).on("click", getApi)
+        getApi();
+        fiveDaysApi();
+    });
+
 }
-/* Click function, it saves searched cities and runs the forecast app.. */
-$("#submitBtn").on("click", function (event) {
-    event.preventDefault();
-    cityPicked = $("#given-input").val().trim();
-    if (!citySearched.includes(cityPicked)) {
-        (citySearched).push(cityPicked);
-    }
-    if (citySearched.length > 5) {
-        citySearched.shift();
-    }
+$(document).ready(render);
